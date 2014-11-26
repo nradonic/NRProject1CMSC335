239ebe88cbfabff6695a1af16c9a30145fee1178
Nick Radonic <big.rad@gmail.com>
2014-11-26 00:20:39 -0500

    Search implemented - copies tree and appends a segment to the bottom of the display screen. with the resultant tree. Not great. Return to string search in toString (), and paste that into the reporting block

--- src/DataTree/FilterField.java
+++
@@ -1,9 +0,0 @@
-package DataTree;
-
-/**
- * FilterField.java
- * @author NickRadonic
- * @since 11/17/14.
- */
-public enum FilterField { NONE, ID, NAME, PARTYID, CREATUREID, CREATURETYPE, AGE, EMPATHY,
-    FEAR, CAPACITY, HEIGHT, WEIGHT, ARTIFACTTYPE, TREASURETYPE, VALUE  }
